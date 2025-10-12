a8732ad9a37e037101aea22175fae61b0b36b9b1
Marco De Luca <marco22874@gmail.com>
2025-10-12 15:46:06 +0200

    Aggiornata versione a 3.0.0 per major refactoring

--- brt_app_pyqt5.py
+++ brt_app_pyqt5.py
@@ -4,9 +4,9 @@ BRT Shipping Management Application - PyQt5
 Converts the LISTADDT.csv file to the format required by BRT
 """
 
-__version__ = "2.15.0"
+__version__ = "3.0.0"
 __app_name__ = "Gestione Spedizioni IGEA <-> BRT"
-__release_date__ = "2025-10-11"
+__release_date__ = "2025-10-12"
 __developer__ = "Marco De Luca"
 
 import sys
